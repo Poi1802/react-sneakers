8af347634aaf7ba4b375e7662eb140ba9e18bc3a
Eugen Poi <you@example.com>
2023-01-26 22:45:57 +0800

    Did Layout :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import Card from "./components/Card";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="header"></div>
+    <div className="wrapper clear">
+      <div style={{ display: "none" }} className="overlay">
+        <Drawer />
+      </div>
+
+      <Header />
+      <div className="content">
+        <div className="content__header d-flex justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="searchBlock d-flex align-center">
+            <img className="mr-10 ml-15" src="/img/search.svg" alt="search" />
+            <input type="text" placeholder="Поиск..." />
+          </div>
+        </div>
+        <div className="cards d-flex flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+const Header = () => {
+  return (
+    <header className="d-flex justify-between p-45">
+      <div className="headerLeft d-flex align-center">
+        <img
+          className="mr-15"
+          width={40}
+          height={40}
+          src="/img/logo.png"
+          alt="logo"
+        />
+        <div className="headerInfo">
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="d-flex align-center">
+        <li className="mr-30">
+          <img className="mr-10" src="/img/cart.svg" alt="cart" />
+          <span>1205 руб.</span>
+        </li>
+        <li className="mr-30">
+          <img src="/img/favourite.svg" alt="" />
+        </li>
+        <li className="mr-20">
+          <img width={20} height={20} src="/img/user.svg" alt="user" />
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
