6a8e7c600e8e50f7a6f15be92dca6198a66b2fd4
Eugen Poi <you@example.com>
2023-01-27 11:06:27 +0800

    Create a Card scss modules

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { card, favorite, button } from "./Card.module.scss";
+
+const Card = ({ title, price, imageUrl }) => {
+  const [checked, setChecked] = React.useState(false);
+  const [liked, setLiked] = React.useState(false);
+
+  const onSelectCheck = () => {
+    setChecked(!checked);
+  };
+  const onSelectLike = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <div className={card}>
+      <div className={favorite} onClick={onSelectLike}>
+        <img
+          src={liked ? "/img/heart-liked.svg" : "/img/heart-unliked.svg"}
+          alt="heart"
+        />
+      </div>
+      <img
+        className="mt-20"
+        width={133}
+        height={112}
+        src={imageUrl}
+        alt="sneakers"
+      />
+      <p>{title}</p>
+      <div className="cardBuy d-flex mb-30">
+        <div className="price mr-40">
+          <span>Цена:</span>
+          <br />
+          <b>{price} руб.</b>
+        </div>
+        <img
+          className={button}
+          onClick={onSelectCheck}
+          src={checked ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
